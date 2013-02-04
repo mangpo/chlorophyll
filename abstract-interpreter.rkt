e91b644d43a7fc529a60937bf1e57f87fa418ae6
Phitchaya Phothilimthana <mangpo@woonsen.(none)>
2013-02-04 11:58:22 -0800

    debug

--- abstract-interpreter.rkt
+++ abstract-interpreter.rkt
@@ -43,7 +43,7 @@
       (dict-set! places place (core space costly-op))
 
       (when debug
-            (pretty-display (format "ADD-SPACE: op = ~a, place = ~a, add = ~a, current =~a" 
+            (pretty-display (format ">> ADD-SPACE: op = ~a, place = ~a, add = ~a, current =~a" 
                                     op place add-space space)))
 
       (when (> space capacity)
@@ -56,6 +56,8 @@
         [(equal? x "any") 0]
         [(equal? y "any") 0]
         [else 
+         (when debug
+               (pretty-display "2-COMM"))
          (inc-space x est-comm)
          (inc-space y est-comm)
          1]))
@@ -67,6 +69,8 @@
     (define/public (visit ast)
       (cond
        [(is-a? ast Num%)
+          (when debug
+                (pretty-display (format "Num ~a" (get-field n ast))))
           (inc-space (get-field place ast) est-num)
           0]
 
@@ -74,27 +78,38 @@
           (define place-known (dict-ref env (get-field name ast) 
                                         (lambda () (send ast not-found-error))))
           (send ast set-place-known place-known)
+
+          (when debug
+                (pretty-display (format "Var ~a" (get-field name ast))))
           (inc-space (get-field place ast) est-var)
           0]
 
        [(is-a? ast UnaExp%)
+          (when debug (newline))
           (define e1 (get-field e1 ast))
           (define e1-count (send e1 accept this))
           (define op-place (get-field place ast))
           (set-field! known-type ast (get-field known-type e1))
-          (inc-space-with-op op-place (get-field op (get-field op ast))) ; increase space
 
+          (when debug
+                (pretty-display (format "UnaOp ~a" (get-field op (get-field op ast)))))
+          (inc-space-with-op op-place (get-field op (get-field op ast))) ; increase space
+          
           (+ e1-count (count-msg op-place (get-field place e1)))]
 
        [(is-a? ast BinExp%)
+          (when debug (newline))
           (define e1 (get-field e1 ast))
           (define e2 (get-field e2 ast))
           (define e1-count (send e1 accept this))
           (define e2-count (send e2 accept this))
           (define op-place (get-field place ast))
           (set-field! known-type ast (and (get-field known-type e1) (get-field known-type e2)))
+
+          (when debug
+                (pretty-display (format "BinOp ~a" (get-field op (get-field op ast)))))
           (inc-space-with-op op-place (get-field op (get-field op ast))) ; increase space
-          
+
           (+ (+ (+ e1-count e2-count)
                       (count-msg op-place (get-field place e1)))
                    (count-msg op-place (get-field place e2)))]
@@ -105,14 +120,20 @@
           (define var-list (get-field var-list ast))
           (for ([var var-list])
                (dict-set! env var place-known))
+
+          (when debug
+                (pretty-display (format "VarDecl ~a" var-list)))
           (inc-space place (* (length var-list) est-data)) ; include space
 
           0]
 
        [(is-a? ast Assign%) 
+          (when debug (newline))
           (define lhs (get-field lhs ast))
           (define rhs (get-field rhs ast))
 
+          (when debug
+                (pretty-display "Assign"))
           ;;; Visit lhs
           (send lhs accept this)
 
@@ -144,7 +165,7 @@
 ;(define test "known int@4 x; x = (-1@1 &@1 100@1) <@4 (!@5 2@5 ||@5 20@5) +@10 -1@10 *@10 2@10;")
 (define test "int@4 x; x = (-1 &@1 x) <@4 (!@5 2 ||@5 20) +@10 -1 *@10 2; x = 1;")
 ;(define my-ast (ast-from-string test))
-(define my-ast (ast-from-file "2.mylang"))
+(define my-ast (ast-from-file "examples/2.mylang"))
 
 (send my-ast pretty-print)
 (define interpreter (new count-msg-interpreter%))
