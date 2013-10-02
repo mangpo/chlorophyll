dd5b77e33d05ed56d5f260fbb7f651484ca199eb
mangpo <mangpo@eecs.berkeley.edu>
2013-10-02 09:56:03 -0700

    add src/test* back

---
+++ src/test-simulator.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+(require "compiler.rkt")
+
+(define result (list))
+
+(define (test name input capacity #:w [w 5] #:h [h 4])
+  (set! result (cons 
+                (cons name (test-simulate name input capacity w h))
+                result)))
+
+;(test "array" "10" 256)
+;(test "simple" "null" 256)
+
+;(test "assign" "null" 512)
+;(test "if"       "4_1" 512)
+;(test "offset"   "4_1" 256)
+;(test "add"       "200" 1024)
+;(test "add2"       "200" 1024)
+;(test "function" "4_1" 256)
+;(test "function" "4_2" 256)
+;(test "function-pair1" "null" 256 #:w 2 #:h 2)
+
+;(test "pair1" "null" 512)
+;(test "pair2" "null" 512)
+;(test "while" "10" 256)
+
+(test "matrixmult6" "72" 400) ;capacity issue
+(test "matrixmult4-dup" "null" 500)
+;(test "cluster" "null" 256)
+
+(test "md5" "null" 600 #:w 10 #:h 5)
+
+(for ([res (reverse result)])
+  (pretty-display res))
---
+++ src/test.rkt
@@ -0,0 +1,56 @@
+#lang s-exp rosette
+
+(require "parser.rkt" 
+         "compiler.rkt"
+         "partitioner.rkt" 
+	 "partition-storage.rkt" 
+	 rackunit)
+
+(define testdir "../tests/")
+
+(define (optimize-file file cores capacity max-msgs)
+  (define my-ast (parse file))
+  (optimize-comm my-ast #:cores cores #:capacity capacity #:max-msgs max-msgs #:verbose #f))
+
+;; Check with expected number of messages
+(define (test-num-msgs name expected-msgs 
+                       #:cores [cores 4] #:capacity [capacity 256] #:max-msgs [max-msgs 8]
+                       [file (string-append testdir name ".cll")])
+  (check-equal? 
+   (result-msgs (optimize-file file cores capacity max-msgs))
+   expected-msgs
+   name)
+  )
+
+;; Consistency Test
+(define (test-consistent name
+                         [cores 4] [capacity 256] [max-msgs 8]
+                         [file1 (string-append testdir name "_concrete.cll")]
+                         [file2 (string-append testdir name "_symbolic.cll")])
+  (let ([res1 (optimize-file file1 cores capacity max-msgs)]
+        [res2 (optimize-file file2 cores capacity max-msgs)])
+  (check-equal? (result-msgs res1) (result-msgs res2))
+    (check-true (cores-equal? (result-cores res1) (result-cores res2)))))
+
+;; No error test
+(define (no-error name [cores 4] [capacity 256] [max-msgs #f]
+                  [file (string-append testdir name ".cll")])
+  (optimize-file file cores capacity max-msgs))
+
+;(test-num-msgs "array-known"   2)
+;(test-num-msgs "array-dynamic" 6)
+;(test-num-msgs "for-array1"    0)
+;(test-num-msgs "for-array2"    560 #:max-msgs 600)
+;(test-num-msgs "for-array3"    0)
+;(test-num-msgs "for-array3-2"  0)
+;(test-num-msgs "for-array4"    120 #:max-msgs 300 #:capacity 512)
+;(test-num-msgs "for-array5"    720 #:max-msgs 800 #:capacity 512)
+;(test-num-msgs "for-array6"    0)
+;(test-num-msgs "add"           100 #:cores 8 #:max-msgs 200 #:capacity 300)
+;(test-num-msgs "add-pair"      100 #:cores 8 #:max-msgs 200 #:capacity 300)
+;(test-num-msgs "function"      2 #:capacity 512)
+(test-num-msgs "function2"     4)
+;(test-num-msgs "while"         300 #:max-msgs 800)
+
+;(test-consistent "space")
+;(test-consistent "if")
